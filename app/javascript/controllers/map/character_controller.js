d04647c988c565967ba31008787e176051b41d49
Tom-Eric Gerritsen <tomeric@eet.nu>
2019-03-17 15:41:03 +0100

    Refactoring of ’map—character’ controller

--- app/javascript/controllers/map/character_controller.js
+++ app/javascript/controllers/map/character_controller.js
@@ -4,16 +4,7 @@ import Draggable from 'lib/map/draggable'
 
 export default class extends Draggable(ObjectController) {
   
-  get floor () {
-    return this._floor || (
-      this._floor =
-        this.application
-            .getControllerForElementAndIdentifier(
-              this.element.closest('[data-controller*="map--floor"]'),
-              'map--floor'
-            )
-    )
-  }
+  get floor () { return this._floor || (this._floor = this.findParentController('map--floor')) }
   
   get canvas ()   { return this.floor.canvas }
   get parent ()   { return this.canvas.viewport }
@@ -27,26 +18,29 @@ export default class extends Draggable(ObjectController) {
     if (!this.floor) return
     
     if (this.spriteURL.length) {
-      this.sprite = PIXI.Sprite.from(this.spriteURL)
-      
-      this.sprite.anchor.set(0)
-      
-      this.sprite.width  = this.width
-      this.sprite.height = this.height
-      this.sprite.x      = this.x
-      this.sprite.y      = this.y
-      
-      this.draw()
+      this.drawSprite()
+      this.setupDraggable(this.sprite)
+      this.enableDragging()
     }
     
-    this.setupDraggable(this.sprite)
-    this.enableDragging()
+    this.draw()
     
     this.floor.updateFieldOfVision()
   }
   
   disconnect () {
     this.undraw()
+  }
+  
+  drawSprite () {
+    this.sprite = PIXI.Sprite.from(this.spriteURL)
+    
+    this.sprite.anchor.set(0)
+    
+    this.sprite.width  = this.width
+    this.sprite.height = this.height
+    this.sprite.x      = this.x
+    this.sprite.y      = this.y
   }
   
   draw() {
