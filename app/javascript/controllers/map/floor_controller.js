2db4d6872accc42bdc31ba853e6f7dea8e17b211
Tom-Eric Gerritsen <tomeric@eet.nu>
2019-03-19 22:35:21 +0100

    Disable hiding the map for now

--- app/javascript/controllers/map/floor_controller.js
+++ app/javascript/controllers/map/floor_controller.js
@@ -64,7 +64,7 @@ export default class extends ApplicationController {
     this.addObstacleLayer()
     this.addGridLayer()
     
-    this.addVisionMask()
+    // this.addVisionMask()
     this.updateFieldOfVision()
   }
   
