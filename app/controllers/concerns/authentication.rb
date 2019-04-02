d7cbd15e7b412b2e5d36e6e0e972318f3508ca8d
Tom-Eric Gerritsen <tomeric@eet.nu>
2019-04-02 20:46:36 +0200

    Always require a player of a game when joining a session

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -16,8 +16,16 @@ module Authentication
   
   def require_user
     if !logged_in?
-      flash[:alert] = "You need to be logged in."
+      flash[:alert] = t('.user_required')
       redirect_to login_url
+      return false
+    end
+  end
+  
+  def require_player
+    if !current_player
+      flash[:alert] = t('.player_required')
+      redirect_to games_url
       return false
     end
   end
--- app/controllers/game_sessions_controller.rb
+++ app/controllers/game_sessions_controller.rb
@@ -1,11 +1,21 @@
 class GameSessionsController < ApplicationController
+  
+  before_action :load_game, except: :index
+  
+  before_action :require_player
+  
   def index
     redirect_to games_url
   end
   
   def show
-    @game = Game.find params[:id]
     @map = @game.maps.first
     @active_floor = @map.floors.first
+  end
+  
+  private
+  
+  def load_game
+    @game = Game.find params[:id]
   end
 end
