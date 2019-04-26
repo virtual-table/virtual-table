d1a3506b91d6c894744419007941fe9313366588
Tom-Eric Gerritsen <tomeric@eet.nu>
2019-04-26 14:12:13 +0200

    Make `map--door` controller work without needing to extend `map--wall` controller

--- app/javascript/controllers/map/door_controller.js
+++ app/javascript/controllers/map/door_controller.js
@@ -1,22 +1,40 @@
-import WallController from 'controllers/map/wall_controller'
+import ObjectController from 'controllers/map/object_controller'
 import PIXI from 'lib/pixi'
 import Rails from '@rails/ujs'
 
-export default class extends WallController {
+export default class extends ObjectController {
+  
+  get room () {
+    return this._room || (this._room = this.findParentController('map--room'))
+  }
+  
+  get floor () {
+    return this.room && this.room.floor
+  }
+  
+  get origin () {
+    return this.data.get('origin').split(',').map((point) => parseFloat(point))
+  }
+  
+  get destination () {
+    return this.data.get('destination').split(',').map((point) => parseFloat(point))
+  }
   
   get closed ()  { return this.data.get('closed') == 'true'      }
   set closed (v) { this.data.set('closed', v ? 'true' : 'false') }
   
   connect () {
     if (!this.floor || !this.floor.gameMasterLayer) return
-    super.connect()
     
-    this.wall.interactive = true
-    this.wall.on('pointerdown', this.toggleDoor.bind(this))
+    this.door = this.floor.gameMasterLayer.addChild(new PIXI.Graphics())
+    this.draw()
+    
+    this.door.interactive = true
+    this.door.on('pointerdown', this.toggleDoor.bind(this))
   }
   
   draw () {
-    if (!this.wall) return
+    if (!this.door) return
     
     let color = this.closed ? 0xFFAA00 : 0x00FF00
     let origin = this.origin
@@ -24,12 +42,12 @@ export default class extends WallController {
     let width = this.destination[0] - this.origin[0]
     let height = this.destination[1] - this.origin[1]
     
-    this.wall.hitArea = new PIXI.Rectangle(
+    this.door.hitArea = new PIXI.Rectangle(
       this.origin[0] - 5, this.origin[1] -5,
       width + 10, height + 10
     )
     
-    this.wall.clear()
+    this.door.clear()
              .lineStyle(4, color, 1)
              .moveTo(...this.origin)
              .lineTo(...this.destination)
