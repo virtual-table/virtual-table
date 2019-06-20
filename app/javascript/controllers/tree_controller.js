19b66f671f676bb87aa8b7c05fb3e4f6ae0e426f
Tom-Eric Gerritsen <tomeric@eet.nu>
2019-06-20 21:41:24 +0200

    Very simple `tree` controller that uses ‘inspire-tree’ to make a “better” tree structure with drag-and-drop support

---
+++ app/javascript/controllers/tree_controller.js
@@ -0,0 +1,62 @@
+import ApplicationController from 'controllers/application_controller'
+import InspireTree from 'inspire-tree'
+import InspireTreeDOM from 'inspire-tree-dom'
+
+export default class extends ApplicationController {
+  
+  static targets = ['input', 'output']
+  
+  // LIFECYCLE:
+  
+  connect () {
+    this.parseList = this.parseList.bind(this)
+    this.parseItem = this.parseItem.bind(this)
+    
+    this.list = this.parseList(this.inputTarget)
+    
+    this.tree = new InspireTree({
+      data: this.list
+    })
+    
+    this.renderer = new InspireTreeDOM(this.tree, {
+      target:      this.outputTarget,
+      dragAndDrop: true
+    })
+    
+    this.inputTarget.style.display = 'none'
+    
+    this.bindEvents()
+  }
+  
+  // DATA:
+  
+  parseList (list) { 
+    const children = list.querySelectorAll(':scope > li')
+    return Array.from(children).map(this.parseItem)
+  }
+  
+  parseItem (item) {
+    const node = item.querySelector('[data-target="tree.node"]')
+    const kids = item.querySelector(':scope > ol, :scope ul')
+    
+    return {
+      text:     node.innerText,
+      element:  node,
+      children: kids ? this.parseList(kids) : []
+    }
+  }
+  
+  // EVENTS:
+  
+  bindEvents () {
+    const tree = this.tree
+    tree.on('node.selected', this.nodeSelected.bind(this))
+  }
+  
+  nodeSelected (node) {
+    if (node.element) {
+      node.element.click()
+    }
+  }
+  
+}
