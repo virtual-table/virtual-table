f8d7412dedddaf471f01e2151ff204505248e288
Tom-Eric Gerritsen <tomeric@eet.nu>
2019-07-12 15:10:22 +0200

    Add a workaround so images can be updated

--- app/models/content/image.rb
+++ app/models/content/image.rb
@@ -8,12 +8,22 @@ module Content
       foreign_type: :content_type
     
     def self.accessible_attributes
-      %i[ file alt_text caption ]
+      %i[ file alt_text caption random_number ]
+    end
     
     def caption_html
       caption.html_safe
     end
     
+    # Unfortunately, only changing the `file` doesn't trigger an update
+    # of the image, so use this as a workaround:
+    def random_number
+      @random_number ||= rand(1_000_000)
+    end
+    
+    def random_number=(new_number)
+      updated_at_will_change! unless new_number == random_number
+      @random_number = new_number
     end
     
   end
