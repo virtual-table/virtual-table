351a0d305da31ba59b186fb1e07833255aedc910
Tom-Eric Gerritsen <tomeric@eet.nu>
2019-07-19 16:07:03 +0200

    Disable polling of localeapp for now

--- config/initializers/localeapp.rb
+++ config/initializers/localeapp.rb
@@ -2,6 +2,9 @@ require 'localeapp/rails'
 
 Localeapp.configure do |config|
   config.api_key = 'ZNWQXdhmrd5dtHezZpGy9GadPcz9qOGVkhTV0FiUJVN1XD2yVK'
+  
+  # Disable polling:
+  config.polling_environments = []
 end
 
 # Pull latest when app restarts in production:
