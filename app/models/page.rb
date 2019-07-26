b4e2f6069d775b21794b3873a8ec73bca0365ebc
Etienne van Delden-de la Haije <etienne@vandelden.family>
2019-07-26 12:21:09 +0200

    clean trailing whitespaces ^_^

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,33 +1,33 @@
 class Page < ApplicationRecord
-  
+
   belongs_to :compendium
-  
+
   belongs_to :parent,
     class_name: 'Page',
     optional: true
-  
+
   has_many :children,
     -> { order(position: :asc) },
     class_name:  'Page',
     foreign_key: 'parent_id',
     dependent:   :destroy
-  
+
   has_many :contents,
     -> { order(position: :asc) },
     class_name: 'PageContent',
     inverse_of: :page,
     autosave:   true,
     dependent:  :destroy
-  
+
   accepts_nested_attributes_for :contents,
     allow_destroy: true
-  
+
   scope :without_parent, -> { where parent: nil }
-  
+
   validates :title, presence: true
-  
+
   before_create :set_default_position
-  
+
   def depth
     if parent
       parent.depth + 1
@@ -35,7 +35,7 @@ class Page < ApplicationRecord
       0
     end
   end
-  
+
   def available_content_types
     PageContent.available_content_types
   end
@@ -43,5 +43,4 @@ class Page < ApplicationRecord
   def set_default_position
     self.position ||= (parent&.children || compendium&.pages || []).size + 1
   end
-  
 end
