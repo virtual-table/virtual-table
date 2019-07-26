c94fc09a1179c13df12bcf8518ce108f70232a1c
Etienne van Delden-de la Haije <etienne@vandelden.family>
2019-07-26 12:24:16 +0200

    Run rubocop autofix

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,26 +1,27 @@
-class Page < ApplicationRecord
+# frozen_string_literal: true
 
+class Page < ApplicationRecord
   belongs_to :compendium
 
   belongs_to :parent,
-    class_name: 'Page',
-    optional: true
+             class_name: 'Page',
+             optional: true
 
   has_many :children,
-    -> { order(position: :asc) },
-    class_name:  'Page',
-    foreign_key: 'parent_id',
-    dependent:   :destroy
+           -> { order(position: :asc) },
+           class_name: 'Page',
+           foreign_key: 'parent_id',
+           dependent: :destroy
 
   has_many :contents,
-    -> { order(position: :asc) },
-    class_name: 'PageContent',
-    inverse_of: :page,
-    autosave:   true,
-    dependent:  :destroy
+           -> { order(position: :asc) },
+           class_name: 'PageContent',
+           inverse_of: :page,
+           autosave: true,
+           dependent: :destroy
 
   accepts_nested_attributes_for :contents,
-    allow_destroy: true
+                                allow_destroy: true
 
   scope :without_parent, -> { where parent: nil }
 
