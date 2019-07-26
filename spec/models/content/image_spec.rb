2939cf164a37453e988dfce54a5dec2a5700b541
Etienne van Delden-de la Haije <etienne@vandelden.family>
2019-07-26 16:51:47 +0200

    Create a spec for the ActiveStorage bug

--- spec/models/content/image_spec.rb
+++ spec/models/content/image_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Content::Image, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:image) { Content::Image.new }
+
+  context 'fix ActiveStorage issue where image is only updated when ANOTHER field is also updated' do
+    context 'when updating random_number' do
+      before { image.random_number = 42 }
+
+      it 'sets updated_at to change' do
+        expect(image.changes).to include 'updated_at'
+      end
+    end
+  end
 end
