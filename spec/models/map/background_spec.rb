9d8966d4ea35ae42478849e545446c21f9700933
Tom-Eric Gerritsen <tomeric@eet.nu>
2019-08-09 16:39:51 +0200

    Add specs for `Map::Background` model

--- spec/models/map/background_spec.rb
+++ spec/models/map/background_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Map::Background, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:background) { build :map_background }
+
+  it { is_expected.to be_valid       }
+  it { is_expected.to be_persistable }
+
+  describe 'relations' do
+    it { is_expected.to belong_to(:floor) }
+  end
 end
