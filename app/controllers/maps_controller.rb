b49e8ece90f2098d1889019d42b8e480023179ec
Tom-Eric Gerritsen <tomeric@eet.nu>
2019-08-09 10:19:37 +0200

    Add page titles to all pages

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -2,6 +2,8 @@ class MapsController < ApplicationController
   
   def show
     @map = Map.find params[:id]
+    @compendium = @map.compendium
+    
     if params[:floor_id]
       @active_floor = @map.floors.find params[:floor_id]
     else
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,44 @@ module ApplicationHelper
       return yield form
     end
   end
+  
+  def site_title
+    'VirtualTable'
+  end
+  
+  def site_subtitle
+    t('.subtitle')
+  end
+  
+  def page_title
+    main_title = strip_tags(title.to_s).presence
+    sub_title  = strip_tags(subtitle.to_s).presence
+    
+    if main_title.present?
+      parts = [main_title, sub_title || site_title]
+    else
+      parts = [site_title, sub_title || site_subtitle]
+    end
+    
+    page_title = parts.compact.join(' - ')
+    page_title.html_safe
+  end
+  
+  # Store the title or return it if no argument is given.
+  def title(new_title = nil)
+    if new_title
+      @page_title = new_title
+    else
+      @page_title || ''
+    end
+  end
+  
+  # Store the title or return it if no argument is given.
+  def subtitle(new_subtitle = nil)
+    if new_subtitle
+      @page_subtitle = new_subtitle
+    else
+      @page_subtitle || ''
+    end
+  end
 end
