7710a1ed97d2541347129397909fd8ea938fe61a
Tom-Eric Gerritsen <tomeric@eet.nu>
2019-08-09 14:09:26 +0200

    Updated specs for `Content::Image` model

--- spec/models/content/image_spec.rb
+++ spec/models/content/image_spec.rb
@@ -1,9 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Content::Image, type: :model do
-  let(:image) { Content::Image.new }
+  subject(:image) { build :content_image }
 
-  context 'fix ActiveStorage issue where image is only updated when ANOTHER field is also updated' do
+  it { is_expected.to be_valid       }
+  it { is_expected.to be_persistable }
+
+  describe 'relations' do
+    it { is_expected.to have_one(:page_content)                }
+    it { is_expected.to have_one(:page).through(:page_content) }
+  end
+
+  describe 'Bugfix: ActiveStorage issue where image is only updated when ANOTHER field is also updated' do
     context 'when updating random_number' do
       before { image.random_number = 42 }
 
