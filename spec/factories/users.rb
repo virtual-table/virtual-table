a9f5f790c95c46ea15885bbb849a94fd3d5e4d47
Tom-Eric Gerritsen <tomeric@eet.nu>
2019-10-13 10:02:09 +0200

    Added specs for `SessionsController` and renamed it’s I18n keys

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,10 @@ FactoryBot.define do
     email    { FFaker::Internet.email    }
     password { FFaker::Internet.password }
     password_confirmation { password }
+    
+    factory :activated_user do
+      activated    { true         }
+      activated_at { Time.now.utc }
+    end
   end
 end
