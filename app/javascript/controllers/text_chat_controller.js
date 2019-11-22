c85a4b0e44a9dd20e1dfe089a7db0d1d6fa7cae9
Tom-Eric Gerritsen <tomeric@eet.nu>
2019-11-22 12:05:45 +0100

    Make server response for chat use the Stimulus controller 'text-chat' and scroll to bottom when new messages arrive

--- app/javascript/controllers/text_chat_controller.js
+++ app/javascript/controllers/text_chat_controller.js
@@ -6,11 +6,27 @@ export default class extends ApplicationController {
   
   static targets = ['messages', 'input']
   
+  connect () {
+    this.scrollToBottom = this.scrollToBottom.bind(this)
+    
+    _.defer(this.scrollToBottom)
+  }
+  
   addMessage (attributes) {
-    let htmlId = `chat_message_${attributes.chat_message_id}`
-    if (!document.getElementById(htmlId)) {
-      this.messagesTarget.innerHTML += attributes.html
+    if (this.hasMessagesTarget) {
+      let htmlId = `chat_message_${attributes.chat_message_id}`
+      
+      if (!document.getElementById(htmlId)) {
+        this.messagesTarget.innerHTML += attributes.html
+        this.scrollToBottom()
+      }
     }
   }
   
+  scrollToBottom () {
+    if (this.hasMessagesTarget) {
+      let container = this.messagesTarget
+      container.scrollTop = container.scrollHeight
+    }
+  }
 }
