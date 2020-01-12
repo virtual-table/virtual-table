86c8d8a1ee3fdd24046a5fc878e17bbcd934408c
Tom-Eric Gerritsen <tomeric@eet.nu>
2020-01-12 11:37:03 +0100

    Rewrite sidebar-tabs controller

--- app/javascript/controllers/sidebar_tabs_controller.js
+++ app/javascript/controllers/sidebar_tabs_controller.js
@@ -2,18 +2,52 @@ import { Controller } from 'stimulus'
 
 export default class extends Controller {
   
-  static targets = ['options', 'tabs']
+  static targets = ['tab', 'pane']
   
-  switchTab () {
-    let tab      = event.currentTarget.getAttribute('data-tab')
-    let isActive = 'is-active'
+  get isActiveClass () { return 'is-active' }
+  
+  connect () {
+    this.activateDefaultTab = this.activateDefaultTab.bind(this)
+    _.defer(this.activateDefaultTab)
+  }
+  
+  activateDefaultTab () {
+    let activeTab = this.tabTargets.find((tab) => tab.classList.contains(this.isActiveClass))
+    let firstTab  = this.tabTargets[0]
+    
+    let defaultTab = activeTab || firstTab
+    
+    if (defaultTab) {
+      this.activateTab(defaultTab)
+    }
+  }
+  
+  switchTab (event) {
+    let currentTab = event.currentTarget
     
     event.preventDefault()
+    this.deactivateAllTabs()
+    this.activateTab(currentTab)
+  }
+  
+  activateTab (tab) {
+    let paneId = tab.dataset.tabTarget
+    tab.classList.add(this.isActiveClass)
     
-    this.optionsTarget.getElementsByClassName(isActive)[0].classList.remove(isActive)
-    event.currentTarget.classList.add(isActive)
+    let panes = this.paneTargets.filter((pane) => pane.dataset.tabPane == paneId)
+    panes.forEach((pane) => pane.classList.add(this.isActiveClass))
+  }
+  
+  deactivateTab (tab) {
+    let paneId = tab.dataset.tabTarget
+    tab.classList.remove(this.isActiveClass)
     
-    this.tabsTarget.getElementsByClassName(isActive)[0].classList.remove(isActive)
-    this.tabsTarget.querySelector('.' + tab).classList.add(isActive)
+    let panes = this.paneTargets.filter((pane) => pane.dataset.tabPane == paneId)
+    panes.forEach((pane) => pane.classList.remove(this.isActiveClass))
+  }
+  
+  deactivateAllTabs () {
+    let activeTabs = this.tabTargets.filter((tab) => tab.classList.contains(this.isActiveClass))
+    activeTabs.forEach((tab) => this.deactivateTab(tab))
   }
 }
